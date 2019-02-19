0c8b29b72233097c0fb01b8a699e0152ad9d0a89
seanemac <S.mccann11@nuigalway.ie>
2019-02-19 21:14:30 +0000

    upload assignment

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,20 +1,21 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.*;
 import java.util.Random;
-import javax.swing.*;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+
+// Sean McCann 16343643
 @SuppressWarnings("serial")
-public class ChatClient extends JFrame {
+public class ChatClient extends JFrame implements Runnable {
 	
     private Container container;
-    private JTextField textField = new JTextField(10);
+    private JTextField textField = new JTextField(30);
     private JTextArea messageArea = new JTextArea(10, 50);
     private JPanel topPanel;
     private JPanel midPanel;
@@ -27,26 +28,43 @@ public class ChatClient extends JFrame {
     private JButton upload;
     private JButton profile;
     private int randomNum;
-	
-	public ChatClient() {
+    private String username;
+    private JFileChooser jfc;
+    
+    private DataOutputStream output;
+    private DataInputStream input;
+    private Socket socket;
+    private int port = 3001;
+    
+    ByteArrayOutputStream baos;
+    BufferedImage imageSend;
+    private File imageFile;
+    
+	public ChatClient() throws IOException {
 		super("Chat");
-		container = getContentPane();
 		
-		// Setup Colour changer
-		Random rand = new Random();
-		randomNum = rand.nextInt(3); // 0-3
+		// Set up GUI
+		container = getContentPane();
 		
 		// Set up buttons
 		send = new JButton("Send");
 		colour = new JButton("Colour");
-		upload = new JButton("Upload");
 		profile = new JButton("Profile");
+		upload = new JButton("Upload");
 		ButtonHandler handler = new ButtonHandler();
 		send.addActionListener(handler);
 		colour.addActionListener(handler);
-		upload.addActionListener(handler);
 		profile.addActionListener(handler);
+		upload.addActionListener(handler);
 		
+		// Setup Colour changer and call the action listener
+		Random rand = new Random();
+		randomNum = rand.nextInt(5); // 0-5
+		colour.doClick();
+		
+		// Set the default button for Enter Key to the Send button
+		SwingUtilities.getRootPane(container).setDefaultButton(send);
+
 		// Font and text areas
 		Font font1 = new Font("SansSerif", Font.BOLD, 20);
 		Font font2 = new Font("SansSerif", Font.BOLD, 18);
@@ -56,15 +74,26 @@ public class ChatClient extends JFrame {
 		
 		// Create image Icon
 		imageLabel = new JLabel();
+		imageLabel.setHorizontalAlignment(JLabel.CENTER);
 		Image img = (new ImageIcon("profileIcon.png")).getImage();
         Image newimg = img.getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH); 
         ImageIcon Icon = new ImageIcon(newimg);
         imageLabel.setIcon(Icon);
         
-        // Top Panel with Image Icon
-        topPanel = new JPanel();
+        // Text input & Send button
+        inputPanel = new JPanel();
+        inputPanel.setOpaque(false);
+    	inputPanel.setLayout(new BorderLayout());
+		inputPanel.add(textField, BorderLayout.CENTER);
+		inputPanel.add(send, BorderLayout.LINE_END);
+		
+        // Top Panel with Image Icon and text input
+		topPanel = new JPanel();
 		topPanel.setOpaque(false);
-        topPanel.add(imageLabel);
+		topPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 20, 20));
+		topPanel.setLayout(new BorderLayout());
+		topPanel.add(imageLabel, BorderLayout.NORTH);
+		topPanel.add(inputPanel, BorderLayout.SOUTH);
         
         // Mid Panel with Text Area
         midPanel = new JPanel();
@@ -73,28 +102,20 @@ public class ChatClient extends JFrame {
 		midPanel.setLayout(new BorderLayout());
 		midPanel.add(messageArea);
 		
-		// Text input & Send button
-        inputPanel = new JPanel();
-        inputPanel.setOpaque(false);
-    	inputPanel.setLayout(new BorderLayout());
-		inputPanel.add(textField, BorderLayout.CENTER);
-		inputPanel.add(send, BorderLayout.LINE_END);
-		
 		// Options Buttons
         optionsPanel = new JPanel();
         optionsPanel.setOpaque(false);
         optionsPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
 		optionsPanel.setLayout(new GridLayout(1,3));
 		optionsPanel.add(colour);
-		optionsPanel.add(upload);
 		optionsPanel.add(profile);
+		optionsPanel.add(upload);
 		
-		// Add the 2 panels to the Bottom
+		// Add the options panel to the bottom
 		bottomPanel = new JPanel();
 		bottomPanel.setOpaque(false);
 		bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 20, 20));
 		bottomPanel.setLayout(new BorderLayout());
-		bottomPanel.add(inputPanel, BorderLayout.NORTH);
 		bottomPanel.add(optionsPanel, BorderLayout.SOUTH);
 		
 		// Add all to container
@@ -105,37 +126,120 @@ public class ChatClient extends JFrame {
 		setSize(600, 700);
 		setVisible(true);
 		
+		// Ask the client for a username before starting
+		username = JOptionPane.showInputDialog(container, "Please sign in with a Username");
+		
+		// Auto select the text field
+		textField.requestFocusInWindow();
 	}
 	
+	// Button Handlers 
 	private class ButtonHandler implements ActionListener {
-		  public void actionPerformed( ActionEvent event )
-		  {
-			  if(event.getActionCommand().equals("Send")) {
-				  // Send
-			  }
-			  else if(event.getActionCommand().equals("Colour")) {
-				  if(randomNum == 0) container.setBackground(Color.green);
-				  else if(randomNum == 1) container.setBackground(Color.yellow);
-				  else if(randomNum == 2) container.setBackground(Color.red);
-				  randomNum = (randomNum + 1) % 3;
-			  }
-			  else if(event.getActionCommand().equals("Upload")) {
-				  // Upload  
-			  }
-			  else if(event.getActionCommand().equals("Profile")) {
-				  // Profile  
-			  }
-		  }
+		public void actionPerformed( ActionEvent event )
+		{
+			// If they hit Enter or press Send
+			if(event.getActionCommand().equals("Send")) {
+				try {
+					// Write the username + text value out to server
+					output.writeUTF(username + ":\t" + textField.getText());
+					textField.setText(null);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			// Originally assigned a random colour, increment colour every time they click
+			else if(event.getActionCommand().equals("Colour")) {
+				if(randomNum == 0) container.setBackground(Color.green);
+				else if(randomNum == 1) container.setBackground(Color.yellow);
+				else if(randomNum == 2) container.setBackground(Color.red);
+				else if(randomNum == 3) container.setBackground(Color.blue);
+				else if(randomNum == 4) container.setBackground(Color.pink);
+				randomNum = (randomNum + 1) % 5;
+			}
+			
+			// Pick a profile picture
+			else if(event.getActionCommand().equals("Profile")) {
+				// Java File Chooser
+				jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+				int returnValue = jfc.showOpenDialog(null);
+				
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+					// Save the selected file for use in 'Upload'
+					imageFile = jfc.getSelectedFile();
+					System.out.println(imageFile.getAbsolutePath());
+					
+					try {
+						// Change the users profile picture to the selected image
+						Image Selected = ImageIO.read(imageFile);
+						Image newimg = Selected.getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
+				        ImageIcon Icon = new ImageIcon(newimg);
+				        imageLabel.setIcon(Icon);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+				}
+			}
+			
+			// Upload image to server
+			else if(event.getActionCommand().equals("Upload")) {
+				if(imageFile == null){
+					// If they have not selected an image
+					JOptionPane.showMessageDialog(container, "Please Select an image first.");
+				}
+				else {
+					try {
+						// Read the image we saved earlier
+						// Write the image to a Byte Array Output Stream
+						imageSend = ImageIO.read(imageFile);
+						baos = new ByteArrayOutputStream();
+						ImageIO.write(imageSend, "jpg", baos);
+						
+						// Let the server know we will send an image next
+						// Write the Byte Array to the server
+						output.writeUTF("IMAGE");
+						output.write(baos.toByteArray());
+						output.flush();
+						
+						// Clear variables for next time
+				        // I think this helped with errors
+				        imageSend = null;
+				        imageFile = null;
+				        JOptionPane.showMessageDialog(container, "Uploaded to Server!");
+				        
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 	}
 	
-	private void run() throws IOException{
+	public void run() {
 		System.out.println("Client Running");
+		try {
+			// Connect to the Server through a socket
+			socket = new Socket("localhost", port);
+			input = new DataInputStream(socket.getInputStream());
+			output = new DataOutputStream(socket.getOutputStream());
+			output.writeUTF(username);
+			// First send them the username
+			while (true) {
+				// Read in any inputs from the server and print them to the messageArea
+				// The whole chat history is sent every time so just set not append
+				String msg = input.readUTF();
+				messageArea.setText(msg);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
 	}
 	
     public static void main(String[] args) throws Exception {
+    	// Create a client and Run it
         ChatClient client = new ChatClient();
         client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        client.setVisible(true);
         client.run();
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,23 +1,37 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Font;
-import java.io.IOException;
+import java.awt.image.BufferedImage;
 
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.UUID;
+import java.io.*;
 
+// Sean McCann 16343643
 @SuppressWarnings("serial")
 public class ChatServer extends JFrame  {
 	
 	private Container container;
-	private JTextArea messageArea = new JTextArea(10, 50);
+	private static JTextArea messageArea = new JTextArea();
 	private JPanel midPanel;
+	
+	private ServerSocket serverSocket;
+	// Store our output streams so that we can write to all of them
+	private static HashSet<DataOutputStream> Dos = new HashSet<DataOutputStream>();
+	// Keep a record of all chat history
+	public volatile static String chatHistory = "";
+	
+	private int port = 3001;
 	
 	public ChatServer() {
 		super("Server");
 		
+		// Set up GUI
 		container = getContentPane();
 		
 		// Set Font
@@ -32,19 +46,106 @@ public class ChatServer extends JFrame  {
 		midPanel.add(messageArea);
 		
 		// Add to container and show
-		container.add(midPanel, BorderLayout.CENTER);
+		container.add(messageArea, BorderLayout.CENTER);
+		messageArea.setText( "Server awaiting connections:\n" );
+		
 		setSize(600, 700);
 		setVisible(true);
 	}
 	
-	private void run() throws IOException{
+	private void run() throws IOException, InterruptedException{
+		// Create a server socket
 		System.out.println("Server Running");
+		serverSocket = new ServerSocket(port);
+	
+		try {
+			// By using a thread we can create multiple client connections
+			while (true) {
+				// Keep waiting for new client connections
+				// If we get a new connection, make a Thread to handle that client and start it
+	            new ClientHandlerThread(serverSocket.accept()).start();
+	        }
+		}
+		finally {
+			// Close the Socket when done
+            serverSocket.close();
+        }
+	}
+	
+	// Thread to Handle each individual client
+	private static class ClientHandlerThread extends Thread {
+		private Socket socket;
+		private byte[] byteArray;
+		private BufferedImage image;
+		
+		public ClientHandlerThread(Socket socket) {
+			this.socket = socket;
+		}
+		
+		public void run() {
+			try {
+				// Create an input and output stream for the client
+				DataInputStream dis = new DataInputStream(socket.getInputStream());
+				DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+				// The client will first send its username so Welcome them
+				String username = dis.readUTF();
+				String welcome = "Welcome to the chat " + username + "\n";
+				// Write the connection to the server TextArea
+				messageArea.append(username + " connected on " + socket.getRemoteSocketAddress()+ "\n");
+				chatHistory += welcome;
+				// Chat history is a complete log of all chats
+				// it is sent back the the client every time and writes over the old text 
+				
+				// Add the output stream to the list of clients, this way we can loop through all of them
+				Dos.add(dos);
+				// Write the old chat history for new clients
+				for (DataOutputStream output : Dos) {
+					output.writeUTF(chatHistory + "\n");
+                }
+				
+				// This loop keeps waiting for inputs from the client
+				while (true) {
+					// Keep reading inputs from the client
+					String input = dis.readUTF();
+					// flag that an image is on its way
+					if(input.startsWith("IMAGE")) {
+						
+						// Prepare and read an image
+						byteArray = new byte[10000000];
+						dis.read(byteArray);
+						image = ImageIO.read(new ByteArrayInputStream(byteArray));
+						
+						// Make the File with the requested format
+						String uniqueID = UUID.randomUUID().toString();
+						String date = new SimpleDateFormat("ddMMyyyy").format(new Date());
+						String filename = uniqueID + "_" + date + ".jpg";
+						File saveFile = new File(filename);
+						
+						// Save the image to the file
+						ImageIO.write(image, "jpg", saveFile);
+						
+						messageArea.append(username + " uploaded an image\n");
+						image = null;
+						byteArray = null;
+					}
+					else {
+						// Not an image, Add the text to the chat history and write back to ALL clients 
+						chatHistory += input + "\n";
+						for (DataOutputStream output : Dos) {
+							output.writeUTF(chatHistory + "\n");
+	                    }
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
     public static void main(String[] args) throws Exception {
+    	// Make a server and Run it
         ChatServer server = new ChatServer();
         server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        server.setVisible(true);
         server.run();
     }
     
